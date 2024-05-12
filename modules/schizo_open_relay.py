b618e4e884ad7301c9e4211f950d36b088d0586c
Alex Vilcacundo <106265492+alex-v505@users.noreply.github.com>
2024-05-12 18:38:56 -0500

    Update schizo_open_relay.py

--- modules/schizo_open_relay.py
+++ modules/schizo_open_relay.py
@@ -1,5 +1,4 @@
-__author__ = '@botnet_hunter'
-
+__author__ = '@alex-v505'
 from datetime import datetime
 import socket
 try:
@@ -20,13 +19,15 @@ sys.path.append("../")
 import mailoney
 
 output_lock = threading.RLock()
-hpc,hpfeeds_prefix = mailoney.connect_hpfeeds()
+hpc, hpfeeds_prefix = mailoney.connect_hpfeeds()
+
 
 def string_escape(s, encoding='utf-8'):
     return (s.encode('latin1')         # To bytes, required by 'unicode-escape'
              .decode('unicode-escape') # Perform the actual octal-escaping decode
              .encode('latin1')         # 1:1 mapping back to bytes
              .decode(encoding))        # Decode original encoding
+
 
 def log_to_file(file_path, ip, port, data):
     with output_lock:
@@ -35,11 +36,13 @@ def log_to_file(file_path, ip, port, data):
             print(file_path + " " + message)
             f.write(message + "\n")
 
+
 def log_to_hpfeeds(channel, data):
-        if hpc:
-            message = data
-            hpfchannel=hpfeeds_prefix+"."+channel
-            hpc.publish(hpfchannel, message)
+    if hpc:
+        message = data
+        hpfchannel = hpfeeds_prefix + "." + channel
+        hpc.publish(hpfchannel, message)
+
 
 def process_packet_for_shellcode(packet, ip, port):
     if libemu is None:
@@ -49,10 +52,12 @@ def process_packet_for_shellcode(packet, ip, port):
     if r is not None:
         # we have shellcode
         log_to_file(mailoney.logpath+"/shellcode.log", ip, port, "We have some shellcode")
-        #log_to_file(mailoney.logpath+"/shellcode.log", ip, port, emulator.emu_profile_output)
-        #log_to_hpfeeds("/shellcode", ip, port, emulator.emu_profile_output)
+        # log_to_file(mailoney.logpath+"/shellcode.log", ip, port, emulator.emu_profile_output)
+        # log_to_hpfeeds("/shellcode", ip, port, emulator.emu_profile_output)
         log_to_file(mailoney.logpath+"/shellcode.log", ip, port, packet)
-        log_to_hpfeeds("shellcode",  json.dumps({ "Timestamp":format(time.time()), "ServerName": self.__fqdn, "SrcIP": self.__addr[0], "SrcPort": self.__addr[1],"Shellcode" :packet}))
+        log_to_hpfeeds("shellcode",  json.dumps({"Timestamp": format(time.time()), "ServerName": mailoney.srvname,
+                                                  "SrcIP": ip, "SrcPort": port, "Shellcode": packet}))
+
 
 def generate_version_date():
     now = datetime.now()
@@ -60,9 +65,11 @@ def generate_version_date():
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     return "{0}, {1} {2} {3} {4}:{5}:{6}".format(week_days[now.weekday()], now.day, months[now.month - 1], now.year, str(now.hour).zfill(2), str(now.minute).zfill(2), str(now.second).zfill(2))
 
+
 __version__ = 'ESMTP Exim 4.69 #1 {0} -0700'.format(generate_version_date())
 EMPTYSTRING = b''
 NEWLINE = b'\n'
+
 
 class SMTPChannel(asynchat.async_chat):
     COMMAND = 0
@@ -82,6 +89,12 @@ class SMTPChannel(asynchat.async_chat):
         self.__data = ''
         from mailoney import srvname
         self.__fqdn = srvname
+        self.__session_info = {
+            'start_time': time.time(),
+            'ip': addr[0],
+            'port': addr[1],
+            'commands': []
+        }
         try:
             self.__peer = conn.getpeername()
         except socket.error as err:
@@ -91,21 +104,17 @@ class SMTPChannel(asynchat.async_chat):
             if err[0] != errno.ENOTCONN:
                 raise
             return
-        #print(>> DEBUGSTREAM, 'Peer:', repr(self.__peer))
-        #self.set_terminator(b'\r\n')
         self.set_terminator(b'\n')
         self.push('220 %s %s' % (self.__fqdn, __version__))
 
-    # Overrides base class for convenience
     def push(self, msg):
         if type(msg) == str:
-            encoded_msg = msg.encode() 
+            encoded_msg = msg.encode()
         elif type(msg) == bytes:
             encoded_msg = msg
 
         asynchat.async_chat.push(self, encoded_msg + self.terminator)
 
-    # Implementation of base class abstract method
     def collect_incoming_data(self, data):
         self.__line.append(data)
         self.__rolling_buffer += data
@@ -114,14 +123,9 @@ class SMTPChannel(asynchat.async_chat):
         process_packet_for_shellcode(self.__rolling_buffer, self.__addr[0], self.__addr[1])
         del data
 
-    # Implementation of base class abstract method
     def found_terminator(self):
-
         line = EMPTYSTRING.join(self.__line).decode()
-        log_to_file(mailoney.logpath+"/commands.log", self.__addr[0], self.__addr[1], string_escape(line))
-        log_to_hpfeeds("commands",  json.dumps({ "Timestamp":format(time.time()), "ServerName": self.__fqdn, "SrcIP": self.__addr[0], "SrcPort": self.__addr[1],"Commmand" : string_escape(line)}))
-
-        #print(>> DEBUGSTREAM, 'Data:', repr(line))
+        self.__session_info['commands'].append(line)
         self.__line = []
         if self.__state == self.COMMAND:
             if not line:
@@ -145,8 +149,6 @@ class SMTPChannel(asynchat.async_chat):
             if self.__state != self.DATA:
                 self.push('451 Internal confusion')
                 return
-            # Remove extraneous carriage returns and de-transparency according
-            # to RFC 821, Section 4.5.2.
             data = []
             for text in line.split('\r\n'):
                 if text and text[0] == '.':
@@ -164,7 +166,6 @@ class SMTPChannel(asynchat.async_chat):
             else:
                 self.push(status)
 
-    # SMTP and ESMTP commands
     def smtp_HELO(self, arg):
         if not arg:
             self.push('501 Syntax: HELO hostname')
@@ -194,15 +195,12 @@ class SMTPChannel(asynchat.async_chat):
             self.push('250 Ok')
 
     def smtp_QUIT(self, arg):
-        # args is ignored
         self.push('221 Bye')
         self.close_when_done()
 
     def smtp_AUTH(self, arg):
-        # Accept any auth attempt
         self.push('235 Authentication succeeded')
 
-    # factored
     def __getaddr(self, keyword, arg):
         address = None
         keylen = len(keyword)
@@ -211,13 +209,10 @@ class SMTPChannel(asynchat.async_chat):
             if not address:
                 pass
             elif address[0] == '<' and address[-1] == '>' and address != '<>':
-                # Addresses can be in the form <person@dom.com> but watch out
-                # for null address, e.g. <>
                 address = address[1:-1]
         return address
 
     def smtp_MAIL(self, arg):
-        #print(>> DEBUGSTREAM, '===> MAIL', arg)
         address = self.__getaddr('FROM:', arg) if arg else None
         if not address:
             self.push('501 Syntax: MAIL FROM:<address>')
@@ -226,11 +221,9 @@ class SMTPChannel(asynchat.async_chat):
             self.push('503 Error: nested MAIL command')
             return
         self.__mailfrom = address
-        #print(>> DEBUGSTREAM, 'sender:', self.__mailfrom)
         self.push('250 Ok')
 
     def smtp_RCPT(self, arg):
-        #print(>> DEBUGSTREAM, '===> RCPT', arg)
         if not self.__mailfrom:
             self.push('503 Error: need MAIL command')
             return
@@ -239,14 +232,12 @@ class SMTPChannel(asynchat.async_chat):
             self.push('501 Syntax: RCPT TO: <address>')
             return
         self.__rcpttos.append(address)
-        #print(>> DEBUGSTREAM, 'recips:', self.__rcpttos)
         self.push('250 Ok')
 
     def smtp_RSET(self, arg):
         if arg:
             self.push('501 Syntax: RSET')
             return
-        # Resets the sender, recipients, and data, but not the greeting
         self.__mailfrom = None
         self.__rcpttos = []
         self.__data = ''
@@ -272,17 +263,12 @@ class SMTPServer(asyncore.dispatcher):
         asyncore.dispatcher.__init__(self)
         try:
             self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-            # try to re-use a server port if possible
             self.set_reuse_addr()
             self.bind(localaddr)
             self.listen(5)
         except:
-            # cleanup asyncore.socket_map before raising
             self.close()
             raise
-        else:
-            pass
-            #print(>> DEBUGSTREAM, '%s started at %s\n\tLocal addr: %s\n\tRemote addr:%s' % (self.__class__.__name__, time.ctime(time.time()), localaddr, remoteaddr))
 
     def handle_accept(self):
         pair = self.accept()
@@ -293,63 +279,52 @@ class SMTPServer(asyncore.dispatcher):
     def handle_close(self):
         self.close()
 
-    # API for "doing something useful with the message"
-    def process_message(self, peer, mailfrom, rcpttos, data, mail_options=None,rcpt_options=None):
-        """Override this abstract method to handle messages from the client.
-
-        peer is a tuple containing (ipaddr, port) of the client that made the
-        socket connection to our smtp port.
-
-        mailfrom is the raw address the client claims the message is coming
-        from.
-
-        rcpttos is a list of raw addresses the client wishes to deliver the
-        message to.
-
-        data is a string containing the entire full text of the message,
-        headers (if supplied) and all.  It has been `de-transparencied'
-        according to RFC 821, Section 4.5.2.  In other words, a line
-        containing a `.' followed by other text has had the leading dot
-        removed.
-
-        This function should return None, for a normal `250 Ok' response;
-        otherwise it returns the desired response string in RFC 821 format.
-
-        """
+    def process_message(self, peer, mailfrom, rcpttos, data, mail_options=None, rcpt_options=None):
         raise NotImplementedError
 
 
-
-def module():
-
-    class SchizoOpenRelay(SMTPServer):
-
-        def process_message(self, peer, mailfrom, rcpttos, data, mail_options=None,rcpt_options=None):
-            #setup the Log File
-            log_to_file(mailoney.logpath+"/mail.log", peer[0], peer[1], '')
-            log_to_file(mailoney.logpath+"/mail.log", peer[0], peer[1], '*' * 50)
-            log_to_file(mailoney.logpath+"/mail.log", peer[0], peer[1], 'Mail from: {0}'.format(mailfrom))
-            log_to_file(mailoney.logpath+"/mail.log", peer[0], peer[1], 'Mail to: {0}'.format(", ".join(rcpttos)))
-            log_to_file(mailoney.logpath+"/mail.log", peer[0], peer[1], 'Data:')
-            log_to_file(mailoney.logpath+"/mail.log", peer[0], peer[1], data)
-
-            loghpfeeds = {}
-            loghpfeeds['ServerName'] = mailoney.srvname
-            loghpfeeds['Timestamp'] = format(time.time())
-            loghpfeeds['SrcIP'] = peer[0]
-            loghpfeeds['SrcPort'] = peer[1]
-            loghpfeeds['MailFrom'] = mailfrom
-            loghpfeeds['MailTo'] = format(", ".join(rcpttos))
-            loghpfeeds['Data'] = data
-            log_to_hpfeeds("mail", json.dumps(loghpfeeds))
+def log_session_info(session_info):
+    if session_info:
+        end_time = time.time()
+        session_duration = end_time - session_info['start_time']
+        log_message = f"Session started at {session_info['start_time']} " \
+                      f"from {session_info['ip']}:{session_info['port']}. " \
+                      f"Duration: {session_duration} seconds. " \
+                      f"Commands: {session_info['commands']}"
+        log_to_file(mailoney.logpath + "/sessions.log", session_info['ip'], session_info['port'], log_message)
 
 
-    def run():
-        honeypot = SchizoOpenRelay((mailoney.bind_ip, mailoney.bind_port), None)
-        print('[*] Mail Relay listening on {}:{}'.format(mailoney.bind_ip, mailoney.bind_port))
-        try:
-            asyncore.loop()
-            print("exiting for some unknown reason")
-        except KeyboardInterrupt:
-            print('Detected interruption, terminating...')
+class SchizoOpenRelay(SMTPServer):
+
+    def process_message(self, peer, mailfrom, rcpttos, data, mail_options=None, rcpt_options=None):
+        log_to_file(mailoney.logpath+"/mail.log", peer[0], peer[1], '')
+        log_to_file(mailoney.logpath+"/mail.log", peer[0], peer[1], '*' * 50)
+        log_to_file(mailoney.logpath+"/mail.log", peer[0], peer[1], 'Mail from: {0}'.format(mailfrom))
+        log_to_file(mailoney.logpath+"/mail.log", peer[0], peer[1], 'Mail to: {0}'.format(", ".join(rcpttos)))
+        log_to_file(mailoney.logpath+"/mail.log", peer[0], peer[1], 'Data:')
+        log_to_file(mailoney.logpath+"/mail.log", peer[0], peer[1], data)
+
+        loghpfeeds = {}
+        loghpfeeds['ServerName'] = mailoney.srvname
+        loghpfeeds['Timestamp'] = format(time.time())
+        loghpfeeds['SrcIP'] = peer[0]
+        loghpfeeds['SrcPort'] = peer[1]
+        loghpfeeds['MailFrom'] = mailfrom
+        loghpfeeds['MailTo'] = format(", ".join(rcpttos))
+        loghpfeeds['Data'] = data
+        log_to_hpfeeds("mail", json.dumps(loghpfeeds))
+
+
+def run():
+    honeypot = SchizoOpenRelay((mailoney.bind_ip, mailoney.bind_port), None)
+    print('[*] Mail Relay listening on {}:{}'.format(mailoney.bind_ip, mailoney.bind_port))
+    try:
+        asyncore.loop()
+        print("exiting for some unknown reason")
+    except KeyboardInterrupt:
+        print('Detected interruption, terminating...')
+
+
+if __name__ == "__main__":
     run()
+
